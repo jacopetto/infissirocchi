0d4202c9589b17e9b258ae7e5403bbe09dff2b6f
jacopetto <jacopo@ntmobile.it>
2024-12-29 18:50:34 +0100

    Primo commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Funzione per mostrare la sezione selezionata
+function showSection(sectionId) {
+    // Ottieni tutte le sezioni
+    const sections = document.querySelectorAll('section');
+    
+    // Rimuovi la classe "active" da tutte le sezioni
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    
+    // Aggiungi la classe "active" alla sezione selezionata
+    const sectionToShow = document.getElementById(sectionId);
+    if (sectionToShow) {
+        sectionToShow.classList.add('active');
+    }
+}
